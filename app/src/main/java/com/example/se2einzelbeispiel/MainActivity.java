bfe6aeb3b9528f3131d27ccf758428c81442a40e
Daniel Kornfeld <dakornfeld@edu.aau.at>
2023-03-05 23:05:16 +0100

    Einbau von Aufgabe 2.2 (3) in der MainActivity.java und UI Anpassung mit zusaetzlichem Button

--- app/src/main/java/com/example/se2einzelbeispiel/MainActivity.java
+++ app/src/main/java/com/example/se2einzelbeispiel/MainActivity.java
@@ -8,11 +8,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     EditText matrikelnummer;
     TextView output;
-    Button button;
+    Button send;
+
+    Button calc;
+
+    int a;
+    int b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +29,58 @@ public class MainActivity extends AppCompatActivity {
 
         matrikelnummer = findViewById(R.id.matrikelnummerInput);
         output = findViewById(R.id.output);
-        button = findViewById(R.id.sendButton);
+        send = findViewById(R.id.sendButton);
+        calc = findViewById(R.id.calcButton);
 
-        button.setOnClickListener(v -> {
+        send.setOnClickListener(v -> {
             Log.d("Matrikelnummer", matrikelnummer.getText().toString());
             output.setText(matrikelnummer.getText());
-
+        });
+        calc.setOnClickListener(v -> {
+            calculateExercise3(String.valueOf(matrikelnummer.getText()));
         });
 
+
     }
+
+    //2.2 Aufgabe 3
+    //Anmerkungen:
+    // - ggt von der Ziffer 0 nicht beruecksichtigt
+    // - die Ziffer wird auf sich selbst nicht als ggt gesehen (bei anderen indexes jedoch schon)
+    // - Doppelnennungen werden herausgestrichen
+    // - Index startet bei 0
+    void calculateExercise3(String matrikelnummer1){
+        List<String> list = new ArrayList<>();
+        int[] matrikelnummerIntArr = new int[matrikelnummer.length()];
+        for (int i = 0; i < matrikelnummer.length(); i++) {
+            matrikelnummerIntArr[i] = matrikelnummer1.charAt(i) - '0';
+        }
+        for (int i = 0; i < matrikelnummerIntArr.length; i++) {
+            for (int j = 0; j < matrikelnummerIntArr.length; j++) {
+                if(matrikelnummerIntArr[i] > 1 && matrikelnummerIntArr[j] > 1) {
+                    a = matrikelnummerIntArr[i];
+                    b = matrikelnummerIntArr[j];
+                    while (a != 0 && b != 0) {
+                        if (a > b) {
+                            a = a - b;
+                        } else {
+                            b = b - a;
+                        }
+                    }
+                    if (a > 1 && i != j && !(list.contains("(" + j + ", " + i + ")"))) {
+                        list.add("(" + i + ", " + j + ")");
+                    }
+                }
+            }
+        }
+        if(!list.isEmpty()){
+            String outputString = list.toString();
+            outputString = outputString.substring(1, outputString.length() - 1);
+            output.setText(outputString);
+        }else{
+            output.setText("");
+        }
+    }
+
+
 }
